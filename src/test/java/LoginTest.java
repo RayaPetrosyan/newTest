a6e0578460a03e0d8a2b5bd62a2b3330f2c7404a
RayaPetrosyan <raya.petrosyan@gmail.com>
2017-12-11 21:11:39 +0400

    seleniumtestproject

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest {
+    private ChromeDriver driver;
+    @BeforeMethod
+    public void setUp ()
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void login()
+    {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword");
+        driver.findElementByCssSelector("#login button").click();
+        assertTrue(driver.findElementByCssSelector(".subheader").isDisplayed());
+
+
+    }
+    @AfterMethod
+    public void tearDown () {
+        driver.close();
+        driver.quit();
+
+    }
+
+}
